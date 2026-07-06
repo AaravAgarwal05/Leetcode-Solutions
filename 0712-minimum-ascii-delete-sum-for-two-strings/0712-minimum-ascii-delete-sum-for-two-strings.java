92929613597ab66e3a49de90ccead576f0136cc0
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-07-06 20:49:14 +0530

    Time: 16 ms (80.28%), Space: 46.4 MB (72.92%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = (int) s1.charAt(i - 1) + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        long total = 0;
+        for(char ch : s1.toCharArray()) {
+            total += (int) ch;
+        }
+        for(char ch : s2.toCharArray()) {
+            total += (int) ch;
+        }
+        return (int) (total - 2 * dp[n][m]);
+    }
+}
