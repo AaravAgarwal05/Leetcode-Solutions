7f95cc0126d07926cb4f3a74bcebdce9aff928e1
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-07-22 02:46:25 +0530

    Time: 60 ms (77.33%), Space: 47.2 MB (98.67%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxActiveSectionsAfterTrade(String s) {
-        char prev = '@';
         int active = 0;
         int j = -1;
         int adj = 0;
+        int len = 0;
         List<Integer> segment = new ArrayList<>();
 
         for(char ch : s.toCharArray()) {
             if(ch == '1') {
                 active++;
+                if(len > 0) {
+                    segment.add(len);
+                }
+                len = 0;
+            } else {
+                len++;
             }
-
-            if(prev != ch && ch == '0') {
-                j++;
-                segment.add(0);
-            }
-
-            if(ch == '0') {
-                segment.set(j, segment.get(j) + 1);
-            }
-
-            prev = ch;
+        }
+        
+        if(len > 0) {
+            segment.add(len);
         }
 
         for(int i = 0; i + 1 < segment.size(); i++) {
