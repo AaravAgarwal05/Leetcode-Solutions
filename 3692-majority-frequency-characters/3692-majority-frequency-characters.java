40e5992710fbdf6c4d902a0eb6b9f70688d81522
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-08-04 19:41:03 +0530

    Time: 5 ms (53.74%), Space: 45.9 MB (19.05%) - LeetHub

---
+++ 3692-majority-frequency-characters/3692-majority-frequency-characters.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String majorityFrequencyGroup(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Integer, List<Character>> group = new HashMap<>();
+
+        for(char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(char ch : map.keySet()) {
+            if(!group.containsKey(map.get(ch))) {
+                group.put(map.get(ch), new ArrayList<>());
+            }
+
+            group.get(map.get(ch)).add(ch);
+        }
+
+        List<Character> maxGroup = new ArrayList<>();
+        int size = 0;
+        int freq = 0;
+
+        for(int f : group.keySet()) {
+            if(group.get(f).size() > size) {
+                maxGroup = group.get(f);
+                size = maxGroup.size();
+                freq = f;
+            } else if(group.get(f).size() == size && f > freq) {
+                maxGroup = group.get(f);
+                size = maxGroup.size();
+                freq = f;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(char ch : maxGroup) {
+            sb.append(ch);
+        }
+
+        return sb.toString();
+    }
+}
