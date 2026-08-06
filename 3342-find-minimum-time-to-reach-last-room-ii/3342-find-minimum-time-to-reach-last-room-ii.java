abcd06d26f07fbe78b60bdc643d328a7bc529c69
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-08-06 23:17:52 +0530

    Time: 332 ms (37.58%), Space: 171.4 MB (63.69%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.java
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.java
@@ -16,15 +16,13 @@ class Solution {
     public int minTimeToReach(int[][] moveTime) {
         int r = moveTime.length;
         int c = moveTime[0].length;
-        int[][][] minWeight = new int[r][c][2];
+        int[][] minWeight = new int[r][c];
 
-        for(int[][] mW1 : minWeight) {
-            for(int[] mW : mW1) {
-                Arrays.fill(mW, Integer.MAX_VALUE);
-            }
+        for(int[] mW : minWeight) {
+            Arrays.fill(mW, Integer.MAX_VALUE);
         }
 
-        minWeight[0][0][0] = 0;
+        minWeight[0][0] = 0;
         PriorityQueue<Tried> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
         pq.add(new Tried(0, 0, 0));
 
@@ -35,27 +33,27 @@ class Solution {
             int weight = curr.weight;
             int p = (x + y) % 2;
 
-            if(weight > minWeight[x][y][p]) {
+            if(weight > minWeight[x][y]) {
                 continue;
             }
 
             for(int[] dir : dirs) {
                 int nx = x + dir[0];
                 int ny = y + dir[1];
-                int nP = (nx + ny) % 2;
 
                 if(nx < 0 || ny < 0 || nx >= r || ny >= c) {
                     continue;
                 }
 
-                int newWeight = Math.max(weight, moveTime[nx][ny]) + (nP == 0 ? 2 : 1);
-                if(newWeight < minWeight[nx][ny][nP]) {
-                    minWeight[nx][ny][nP] = newWeight;
+                int newWeight = Math.max(weight, moveTime[nx][ny]) + (p == 0 ? 1 : 2);
+                
+                if(newWeight < minWeight[nx][ny]) {
+                    minWeight[nx][ny] = newWeight;
                     pq.add(new Tried(nx, ny, newWeight));
                 }
             }
         }
 
-        return Math.min(minWeight[r - 1][c - 1][0], minWeight[r - 1][c - 1][1]);
+        return minWeight[r - 1][c - 1];
     }
 }
