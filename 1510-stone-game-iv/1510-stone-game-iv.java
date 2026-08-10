0e3bddf735dad1bd82a24916765fa3ab7ad03673
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-08-10 21:11:31 +0530

    Time: 304 ms (5.22%), Space: 46.5 MB (50.65%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-        Set<Integer> squares = new HashSet<>();
+        List<Integer> squares = new ArrayList<>();
         boolean[] dp = new boolean[n + 1];
 
         for(int i = 1; i * i <= n; i++) {
@@ -10,7 +10,7 @@ class Solution {
         for(int i = 1; i <= n; i++) {
             for(int square : squares) {
                 if(square > i) {
-                    continue;
+                    break;
                 }
                 
                 if(!dp[i - square]) {
