769ac79e387eb237fe914fcbcc3857ec6a8cb17b
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-08-20 02:42:40 +0530

    Time: 4 ms (75.18%), Space: 82.4 MB (78.02%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -1,35 +1,35 @@
 class Solution {
     public int diagonalPrime(int[][] nums) {
         int n = nums.length;
-        final int maxLimit = (int)(4 * 1e6);
-        boolean[] isNotPrime = new boolean[maxLimit + 1];
-        isNotPrime[0] = true;
-        isNotPrime[1] = true;
         int maxPrime = 0;
-
-        for(int i = 2; i * i <= maxLimit; i++) {
-            if(isNotPrime[i]) {
-                continue;
-            }
-
-            for(int p = i * i; p <= maxLimit; p += i) {
-                isNotPrime[p] = true;
-            }
-        }
 
         for(int i = 0; i < n; i++) {
             int num1 = nums[i][i];
             int num2 = nums[i][n - 1 - i];
 
-            if(!isNotPrime[num1] && num1 > maxPrime) {
+            if(isPrime(num1) && num1 > maxPrime) {
                 maxPrime = num1;
             }
 
-            if(!isNotPrime[num2] && num2 > maxPrime) {
+            if(isPrime(num2) && num2 > maxPrime) {
                 maxPrime = num2;
             }
         }
 
         return maxPrime;
+    }
+
+    private boolean isPrime(int n) {
+        if(n < 2) {
+            return false;
+        }
+
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
