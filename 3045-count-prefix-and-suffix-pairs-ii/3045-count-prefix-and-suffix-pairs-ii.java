ba46aecff679b7af3580aed6335179a7451f88ce
Aarav Agarwal <149526514+AaravAgarwal05@users.noreply.github.com>
2026-08-29 03:46:44 +0530

    Time: 141 ms (32.08%), Space: 283.8 MB (8.49%) - LeetHub

---
+++ 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.java
@@ -0,0 +1,50 @@
+class Solution {
+     private class Node {
+        Map<Integer, Node> map;
+        int count;
+
+        Node() {
+            map = new HashMap<>();
+            count = 0;
+        }
+    }
+
+    private class Trie {
+        Node root;
+
+        Trie() {
+            root = new Node();
+        }
+
+        public int insert(String str) {
+            int size = str.length();
+            Node curr = root;
+            int sum = 0;
+
+            for(int i = 0; i < size; i++) {
+                int key = (str.charAt(i) << 8) | str.charAt(size - i - 1);
+
+                if(!curr.map.containsKey(key)) {
+                    curr.map.put(key, new Node());
+                }
+
+                curr = curr.map.get(key);
+                sum += curr.count;
+            }
+
+            curr.count++;
+            return sum;
+        }
+    }
+
+    public long countPrefixSuffixPairs(String[] words) {
+        Trie trie = new Trie();
+        long count = 0;
+
+        for(String word : words) {
+            count += trie.insert(word);
+        }
+
+        return count;
+    }
+}
